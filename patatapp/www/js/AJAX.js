bd556c0a01ed6a1a94ac250aba00cf94bcd257c5
Loïc Bouix <loic.bouix@iut-valence.fr>
2014-12-05 03:16:33 +0100

    Test ajax

---
+++ patatapp/www/js/AJAX.js
@@ -0,0 +1,39 @@
+// Fonction retournant un XMLHttpRequest, (légèrement) adaptée pour tenir compte du navigateur
+function getXHR() {
+   var res = null;     
+   if (window.XMLHttpRequest) // Firefox 
+      res = new XMLHttpRequest(); 
+   else if (window.ActiveXObject) // Internet Explorer 
+      res = new ActiveXObject("Microsoft.XMLHTTP"); 
+   // else XMLHttpRequest non supporté ?
+   return res; 
+}
+
+// L'objet XMLHttpRequest, global pour la fonction callback
+var xhr = getXHR();
+
+// Mise à jour de la liste dès que les données sont disponibles
+function retourRequete() {
+   if (xhr.readyState == xhr.DONE) { // Données disponibles
+      var a = document.getElementById("success");
+
+      a.setAttribute.innerHTML = xhr.responseText;
+	  
+      if (a.hasAttribute("style"))
+         a.removeAttribute("style");
+   }
+}
+
+// Chargement de la liste des plats d'un type
+function envoyerFormulaire() {
+   // On vérifie qu'un type est sélectionné
+   var champ = document.getElementById("test");
+
+    // Ouverture d'une connexion en mode asynchrone
+    xhr.onreadystatechange = retourRequete;
+    // Avec POST
+    xhr.open("POST", "http://85.14.137.6/projets/NI2014-AppMobile/serveur.php", true);
+    var param = 'test=' + champ.value;
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(param);
+}
