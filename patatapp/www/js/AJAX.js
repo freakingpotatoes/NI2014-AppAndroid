a0a3c07f43d6ff934930ecc064ad2aaaa5e1dc6f
Loïc Bouix <loic.bouix@iut-valence.fr>
2014-12-05 05:59:14 +0100

    Correction transmission

--- patatapp/www/js/AJAX.js
+++ patatapp/www/js/AJAX.js
@@ -15,7 +15,7 @@ var xhr = getXHR();
 // Mise à jour de la liste dès que les données sont disponibles
 function retourRequete() {
    if (xhr.readyState == xhr.DONE) { // Données disponibles
-	  if(xhr.responseText == 'ok') {
+	  if(xhr.responseText == 'OK Location') {
 		alert('ok!');
 	  }
 	  else {
@@ -33,7 +33,7 @@ function envoyerFormulaire() {
     // Avec POST
     xhr.open("POST", "http://85.14.137.6/projets/test1/Tests/patatApp.php", true);
 	
-    var param = 'addLocation=1&longitude='+longitude.value+'&latitude='+latitude.value+'&id_state=FR';
+    var param = 'addLocation=1&longitude='+longitude.value+'&latitude='+latitude.value+'&id_state=FR&address=IUT&city=Valence&name=Loic&phone=0651217566&mail=loic.bouix@iut-valence.fr';
 	
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send(param);
