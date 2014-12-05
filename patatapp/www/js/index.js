2b1d3a341a7f0dab2d95858d484ac88d181d4d2e
Loïc Bouix <loic.bouix@iut-valence.fr>
2014-12-05 04:53:43 +0100

    Ajout map

--- patatapp/www/js/index.js
+++ patatapp/www/js/index.js
@@ -19,7 +19,31 @@
 alert('Ok');
 
 function success(position) {
-  alert(position.coords.latitude + "\n" + position.coords.longitude);
+  var mapcanvas = document.createElement('div');
+  mapcanvas.id = 'mapcontainer';
+  mapcanvas.style.height = '400px';
+  mapcanvas.style.width = '600px';
+
+  document.querySelector('article').appendChild(mapcanvas);
+
+  var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  
+  var options = {
+    zoom: 15,
+    center: coords,
+    mapTypeControl: false,
+    navigationControlOptions: {
+    	style: google.maps.NavigationControlStyle.SMALL
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
+
+  var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title:"Vous etes là"
+  });
 }
 
 function error(error) {
